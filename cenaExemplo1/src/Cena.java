f6fa19e8ff3a09b1df5b785aa838611b0ded2616
Gabriel Paglia <126928543+Gerib0y@users.noreply.github.com>
2023-11-23 21:41:26 -0300

    Add files via upload

--- cenaExemplo1/src/Cena.java
+++ cenaExemplo1/src/Cena.java
@@ -4,9 +4,10 @@ import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.GLAutoDrawable;
 import com.jogamp.opengl.GLEventListener;
 import com.jogamp.opengl.glu.GLU;
+import com.jogamp.opengl.util.gl2.GLUT;
 import com.jogamp.opengl.util.awt.TextRenderer;
-
 import java.awt.*;
+import java.util.Locale;
 import java.util.Random;
 
 public class Cena implements GLEventListener{
@@ -26,14 +27,16 @@ public class Cena implements GLEventListener{
     private float limiteSuperior = 1.0f, limiteEsquerdo = -1.8f, limiteDireito = 1.8f, limiteInferior = -0.85f; // Limites da Bola na Tela
     private float velocidadeBola = 0.01f; // Velocidade da Bola
     private float direcaoBolaX = 0.9f, direcaoBolaY = -0.9f; // Direção da Bola
-    public float plataformaX = 0.0f, plataformaY = -0.99f; // Posição inicial da plataforma
-    private float larguraPlataforma = 0.6f, alturaPlataforma = 0.1f; // Plataforma
+    public float plataformaY = -0.99f; // Posição inicial da plataforma
+    public float userBarMove = 0;
     // Texto
     private TextRenderer textRenderer;
+    private GL2 gl;
 
     @Override
     public void init(GLAutoDrawable drawable) {
         //dados iniciais da cena
+        gl = drawable.getGL().getGL2();
         glu = new GLU();
         //Estabelece as coordenadas do SRU (Sistema de Referencia do Universo)
         xMin = yMin = zMin = -1;
@@ -53,27 +56,10 @@ public class Cena implements GLEventListener{
 
         switch (op) {
             case 1:
-                gl.glPushMatrix();
-                desenhaTexto(gl, 630, 900, Color.CYAN, "PONG");
-                desenhaTexto(gl, 50, 600, Color.WHITE, "COMANDOS:");
-                desenhaTexto(gl, 50, 550, Color.WHITE, "As teclas ← e → movem a barra!");
-                desenhaTexto(gl, 50, 500, Color.WHITE, "Aperte S para iniciar!");
-                desenhaTexto(gl, 50, 450, Color.WHITE, "Aperte M para voltar ao menu principal!");
-
-                gl.glPopMatrix();
+                menu();
                 break;
             case 2:
-                // Desenha a Plataforma
-                gl.glPushMatrix();
-                gl.glTranslatef(translacao, 0.05f, 0);
-                criaRetangulo(gl);
-                gl.glPopMatrix();
-
-                //Desenha a Bola
-                gl.glPushMatrix();
-                gl.glTranslatef(0, 0, 0);
-                criaBola(gl);
-                gl.glPopMatrix();
+                fase1();
                 break;
 
             default:
@@ -82,7 +68,67 @@ public class Cena implements GLEventListener{
         }
 
         gl.glFlush();
+    }
 
+    public void menu() {
+        gl.glPushMatrix();
+            desenhaTexto(gl, 550, 900, Color.CYAN, "-=PONG=-");
+            desenhaTexto(gl, 50, 600, Color.WHITE, "COMANDOS:");
+            desenhaTexto(gl, 50, 550, Color.WHITE, "As teclas ← e → movem a barra!");
+            desenhaTexto(gl, 50, 500, Color.WHITE, "Aperte S para iniciar!");
+            desenhaTexto(gl, 50, 450, Color.WHITE, "Aperte M para voltar ao menu principal!");
+        gl.glPopMatrix();
+    }
+
+    public void fase1() {
+        // Desenha a Barra
+        gl.glPushMatrix();
+        gl.glTranslatef(translacao, 0.05f, 0);
+        criaRetangulo(gl);
+        gl.glPopMatrix();
+
+        // Desenha a Bola
+        gl.glPushMatrix();
+        gl.glTranslatef(0, 0, 0);
+        criaBola(gl);
+        gl.glPopMatrix();
+        bolaMove();
+    }
+
+    public void desenhaTexto(GL2 gl, int xPosicao, int yPosicao, Color cor, String frase){
+        gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_FILL);
+        //Retorna a largura e altura da janela
+        textRenderer.beginRendering(Renderer.screenWidth, Renderer.screenHeight);
+        textRenderer.setColor(cor);
+        textRenderer.draw(frase, xPosicao, yPosicao);
+        textRenderer.endRendering();
+        gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, mode);
+    }
+    public void criaRetangulo(GL2 gl){
+        gl.glPushMatrix();
+            gl.glTranslatef(userBarMove, 0, 0);
+            gl.glBegin(GL2.GL_QUADS);
+                gl.glColor3f(0f, 1f, 1f);
+                gl.glVertex2f(-0.2f, -0.8f);
+                gl.glColor3f(0.0f, 0.0f, 0.0f);
+                gl.glVertex2f(0.2f, -0.8f);
+                gl.glColor3f(0.0f, 1f, 1f);
+                gl.glVertex2f(0.2f, -0.9f);
+                gl.glColor3f(0.0f, 0.0f, 0.0f);
+                gl.glVertex2f(-0.2f, -0.9f);
+            gl.glEnd();
+        gl.glPopMatrix();
+    }
+    public void criaBola(GL2 gl) {
+        gl.glColor3d(1, 1, 1);
+        gl.glBegin(GL2.GL_POLYGON);
+        for (double i = 0; i < limite; i += 0.01) {
+            gl.glVertex2d(cX + rX * Math.cos(i), cY + rY * Math.sin(i));
+        }
+        gl.glEnd();
+    }
+
+    public void bolaMove() {
         cX += direcaoBolaX * velocidadeBola;
         cY += direcaoBolaY * velocidadeBola;
 
@@ -96,16 +142,6 @@ public class Cena implements GLEventListener{
         if (cY + rY > limiteSuperior) {
             direcaoBolaY = -direcaoBolaY; // Inverte a direção vertical
         }
-        if (verificaColisaoBolaComPlataforma()) {
-            direcaoBolaY = -direcaoBolaY;
-        }
-    }
-
-    private boolean verificaColisaoBolaComPlataforma() {
-        return (cX + rX > plataformaX - 1.0f  &&
-                //cX - rX < plataformaX + 0.2f &&
-                //cY + rY > plataformaY - alturaPlataforma &&
-                cY - rY < plataformaY + alturaPlataforma );
     }
 
     @Override
@@ -137,33 +173,6 @@ public class Cena implements GLEventListener{
         gl.glMatrixMode(GL2.GL_MODELVIEW);
         gl.glLoadIdentity(); //lê a matriz identidade
         System.out.println("Reshape: " + width + ", " + height);
-    }
-
-    public void desenhaTexto(GL2 gl, int xPosicao, int yPosicao, Color cor, String frase){
-        gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_FILL);
-        //Retorna a largura e altura da janela
-        textRenderer.beginRendering(Renderer.screenWidth, Renderer.screenHeight);
-        textRenderer.setColor(cor);
-        textRenderer.draw(frase, xPosicao, yPosicao);
-        textRenderer.endRendering();
-        gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, mode);
-    }
-    public void criaRetangulo(GL2 gl){
-        gl.glColor3f(0, 1, 1f); // Cor verde
-        gl.glBegin(GL2.GL_QUAD_STRIP);
-        gl.glVertex2f(plataformaX - larguraPlataforma / 2, plataformaY); // Canto inferior esquerdo
-        gl.glVertex2f(plataformaX + larguraPlataforma / 2, plataformaY); // Canto inferior direito
-        gl.glVertex2f(plataformaX - larguraPlataforma / 2, plataformaY + 0.1f); // Canto superior esquerdo
-        gl.glVertex2f(plataformaX + larguraPlataforma / 2, plataformaY + 0.1f); // Canto superior direito
-        gl.glEnd();
-    }
-    public void criaBola(GL2 gl) {
-        gl.glColor3d(1, 1, 1);
-        gl.glBegin(GL2.GL_POLYGON);
-        for (double i = 0; i < limite; i += 0.01) {
-            gl.glVertex2d(cX + rX * Math.cos(i), cY + rY * Math.sin(i));
-        }
-        gl.glEnd();
     }
 
     @Override
